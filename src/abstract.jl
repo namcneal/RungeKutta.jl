3369a21e791e7599da1fd910f23d6bb1066e6140
pass <pass@gmail.com>
2022-10-23 20:01:30 -0400

    idk

--- src/abstract.jl
+++ src/abstract.jl
@@ -1,6 +1,4 @@
-using NSDEBase:  AbstractInitialValueSolver
-
-abstract type AbstractRungeKuttaSolver <: AbstractInitialValueSolver end
+abstract type AbstractRungeKuttaSolver <: NSDEBase.AbstractInitialValueSolver end
 abstract type AbstractRungeKuttaSolution <: AbstractInitialValueSolution end
 abstract type AbstractRungeKuttaParameters <: AbstractInitialValueParameters end
 abstract type AbstractRungeKuttaCache <: AbstractInitialValueCache end
