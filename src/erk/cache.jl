e8b8619873d297072fd5042f8a52f8c68b841580
pass <pass@gmail.com>
2022-10-25 23:44:14 -0400

    Needed another cache for the way I'm doing RK

--- src/erk/cache.jl
+++ src/erk/cache.jl
@@ -2,6 +2,7 @@ mutable struct ExplicitRungeKuttaCache{n_T<:Integer, m_T<:Integer, v_T<:(Abstrac
     n::n_T # step counter
     m::m_T # adaptive counter
     v::v_T # avoids allocation inside `adaptivestep!`
+    v2::v_T
     k::k_T # stages at step `n`
     e::e_T # compensated summation error
 end
@@ -9,7 +10,8 @@ end
 function ExplicitRungeKuttaCache(u0::AbstractVector{ℂ}, solver::ExplicitRungeKuttaSolver) where {ℂ<:Number}
     @↓ s = solver.tableau
     n = m = 1
-    v = similar(u0)
+    v  = similar(u0)
+    v2 = simular(u0)
     k = [similar(u0) for i = 1:s]
     e = Ref(0.0)
     return ExplicitRungeKuttaCache(n, m, v, k, e)
@@ -19,7 +21,8 @@ function ExplicitRungeKuttaCache(problem::AbstractInitialValueProblem, solver::E
     @↓ u0 = problem
     @↓ s = solver.tableau
     n = m = 1
-    v = similar(u0)
+    v  = similar(u0)
+    v2 = similar(u0)
     k = [similar(u0) for i = 1:s]
     e = Ref(0.0)
     return ExplicitRungeKuttaCache(n, m, v, k, e)
