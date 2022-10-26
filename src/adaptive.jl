3f0c8be8534c107fb99fa925476bc6f9ec6e2291
pass <pass@gmail.com>
2022-10-26 01:40:42 -0400

    Changed the way the desired tolerance for the adaptive RK is determined to make it more general.

--- src/adaptive.jl
+++ src/adaptive.jl
@@ -13,10 +13,9 @@ AdaptiveParameters(; atol::Real=0.0, rtol::Real=1e-5, nits::Integer=100)
 - `rtol :: Real` : relative tolerance.
 - `nits :: Integer` : maximum number of iterations.
 """
-struct AdaptiveParameters{atol_T<:Real, rtol_T<:Real, nits_T<:Integer} <: AbstractAdaptiveParameters
-    atol::atol_T
-    rtol::rtol_T
-    nits::nits_T
+struct AdaptiveParameters <: AbstractAdaptiveParameters
+	desired_tol::Real
+	max_num_iters::Int64
 end
 
-AdaptiveParameters(; atol::Real=0.0, rtol::Real=1e-5, nits::Integer=100) = AdaptiveParameters(atol, rtol, nits)
+AdaptiveParameters(; desired_tol::Float64) = AdaptiveParameters(desired_tol, 100)
